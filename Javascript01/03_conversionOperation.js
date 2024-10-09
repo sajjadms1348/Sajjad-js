7ed0e94df6bea6537901e5d490133219b9313ce5
sajjadms1348 <sajjadms1348@gmail.com>
2024-10-09 12:03:00 +0000

    conversionof datatypes

---
+++ Javascript01/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "45abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "45" => 45 it will convert to numbers from string
+// "45abc" => it will convert to numbers and value will show as NaN
+// Boolean => true = 1 and false =0
+
+let isLoggedIn = "sajjad"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 11
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
