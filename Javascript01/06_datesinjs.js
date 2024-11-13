80fc4efe9dc2aee13d0aa2beb97d1b16f74a6028
sajjadms1348 <sajjadms1348@gmail.com>
2024-11-13 06:21:58 +0000

    DateInJS

---
+++ Javascript01/06_datesinjs.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2023, 6, 20, 6, 44)
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+
+
