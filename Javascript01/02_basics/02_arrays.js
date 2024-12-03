c42d56f9cf55741a5bdc306ef0ece8c9baacb49d
sajjadms1348 <sajjadms1348@gmail.com>
2024-12-03 12:34:41 +0000

    array02update

---
+++ Javascript01/02_basics/02_arrays.js
@@ -0,0 +1,15 @@
+const heros = ["Sharukh", "Salman", "Amir"]
+const heroin = ["Deepika", "Katrina", "Bebo"]
+
+//heros.push(heroin)
+//console.log(heros);
+const newall = heros.concat(heroin)
+console.log(newall);
+
+
+const Array1 = [1,2,3,4,5];
+const Array2 = [5,6,7,8,9,10];
+const Array3 = [0,0,0];
+const Array4 = [1,3,9,19];
+const concatAll = Array1.concat(Array2, Array3,Array4)
+console.log(concatAll)
